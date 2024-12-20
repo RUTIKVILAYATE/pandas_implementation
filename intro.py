c8dc07a3e920c4eb53d98fa03f543f36b3cc4034
RUTIKVILAYATE <rutik.vilayate.ai@gmail.com>
2024-12-20 22:28:23 +0530

    intro

---
+++ intro.py
@@ -0,0 +1,17 @@
+# what is pandas?
+
+# In computer programming, pandas is a software library 
+# written for the python programming language for data
+# manipulation and analysis.
+
+# In particular, it offers data structures and operations
+# for manipulating numerical tables and time series
+
+
+
+# Pandas main Data-Structures
+
+# Dataframes    -> excel file or tabular data
+# Series        -> extracting a column or a row from the table 
+
+
